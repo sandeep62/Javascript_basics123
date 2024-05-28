d85fbddda524b3dd70773463c1ed56c9927a66b7
sandeep62 <109506135+sandeep62@users.noreply.github.com>
2024-05-28 12:24:41 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,20 @@
+let name="sandeep"
+console.log(typeof(name));
+
+//types of data type
+/*
+stirng 
+boolean
+big int 
+number
+synbol
+undefined
+
+*/
+
+//important
+/*
+null is not a datatype ,its object
+undefind datatype is undefined 
+
+*/
