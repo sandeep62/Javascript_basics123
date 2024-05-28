cdaffd9777ae09b5f4ed149ccfacdd47d390dccf
sandeep62 <109506135+sandeep62@users.noreply.github.com>
2024-05-28 12:48:26 +0000

    typeconversion

---
+++ basics/typeconversion.js
@@ -0,0 +1,19 @@
+let score="sandeep";
+// console.log(typeof(score))
+
+ let check=Number(score);
+// console.log(typeof check)
+
+// if it is null value 
+
+
+// number=> number
+//in boolean conversion 
+// " "=> false ,"hin"=>true ,1=>true, 0=> false 
+  let someNumber=23;
+  console.log(someNumber)
+
+  let typechange=String(someNumber)
+  console.log(typechange);
+  console.log(typeof(typechange))
+
