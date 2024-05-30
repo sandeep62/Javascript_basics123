26be4176e60fadd6390c2ea077132e2d967146e5
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-05-30 13:24:01 +0000

    slice_splice

---
+++ part2basics/slicesplics.js
@@ -0,0 +1,12 @@
+const myArr=[1,2,3,4,5]
+
+//slice
+const myArr1=myArr.slice(1,3);
+console.log("A",myArr);
+console.log(`slice is ${myArr1}`);
+
+
+//splice
+const myArr2=myArr.splice(1,3)
+console.log("b",myArr);
+console.log(`splice is this ${myArr2}`);
