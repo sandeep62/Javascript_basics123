3167e282c63e52c6e783fedd2763d44c4fe89c89
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-03 12:41:52 +0000

    test

--- part2basics/array.js
+++
@@ -1,6 +0,0 @@
-const Marvel=["ironman","thor",["sandeep","jatan","anjali",["rahul"]],"Captain"]
-const Dc=["superman","batman"]
-const all=[...Dc,...Marvel]  //spread operator 
-// console.log(all);
-
-console.log(all.flat(Infinity)) // flat operator is widely used to spread the multiple inner array .
