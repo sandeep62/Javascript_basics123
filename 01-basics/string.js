8ca8e466f92774b12f9e8589d18bcb541ee0a0c6
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-03 12:39:45 +0000

    string

---
+++ 01-basics/string.js
@@ -0,0 +1,10 @@
+const myNum=100.234;
+console.log(myNum.toFixed(1));
+// to fix the decimal number after real number
+
+
+console.log(myNum.toPrecision(3));
+
+
+const Num=100000000000;
+console.log(Num.toLocaleString('en-IN'));
