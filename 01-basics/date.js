1baf973879fba33910b12009edfd1851d2ff8e65
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-03 12:39:27 +0000

    string

---
+++ 01-basics/date.js
@@ -0,0 +1,8 @@
+let mydate=new Date(2023,0,23)
+console.log(mydate.toDateString());
+
+
+let mytime=new Date()
+console.log(Math.floor(Date.now()/1000)); // to convert it into  second divide by 1000.
+
+console.log(mytime.getFullYear());
