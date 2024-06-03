4be308ad656fde517dec3cf3a3213f10aa17c1b8
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-03 12:41:17 +0000

    scope

---
+++ 03-basics/scope.js
@@ -0,0 +1,15 @@
+if(true)
+    {
+        const username="sandeep"
+        if(username=="sandeep")
+            {
+                const website="youtube"
+                console.log(username+website);
+            }
+            // console.log(website);
+
+    }
+
+    // child function can call parent element but parent element cant access the child element 
+    
+    
