194e65f80bb3b328babc5262986ea104dc1453ab
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-04 13:49:09 +0000

    foreach

---
+++ 06-loop/foreach.js
@@ -0,0 +1,36 @@
+// it is specially used in arrray 
+
+// const coding =["python ","js"," html"," css"]
+
+// coding.forEach(function(item){
+//     console.log(item);
+// })
+
+
+const mycoding=[
+    {
+        language:"javascript ",
+        languageshort: " js"
+    },
+    {
+        language:"python",
+        languageshort: " py"
+    },
+    {
+        language:"c++ ",
+        languageshort: " cpp"
+    },
+    {
+        language:"typescript  ",
+        languageshort: "type"
+    },
+    {
+        language:"golang ",
+        languageshort: " go"
+    },
+]
+mycoding.forEach((item)=>{
+    console.log(item.languageshort);
+})
+//it is the best way to access the multiple object inside the array 
+
