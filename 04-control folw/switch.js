921cf700115470555d758f79f16c1c3f4bba9ffa
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-04 12:40:16 +0000

    switch

---
+++ 04-control folw/switch.js
@@ -0,0 +1,24 @@
+const month=4;
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    default:
+        console.log("default value is matched");
+        break;
+}
