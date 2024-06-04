98a8c19d64e978c40d7bf24de3d2a01d40d4f057
San_sin <109506135+sandeep62@users.noreply.github.com>
2024-06-04 13:48:58 +0000

    filter

---
+++ 06-loop/filter.js
@@ -0,0 +1,27 @@
+// const nums=[1,2,3,4,5,6,7,8]
+// const mynum=nums.filter((item)=>{ // in filter if scope is initiated then we have
+    
+//     return item>4;  // to put return otherwise it will show empty array
+// })
+// console.log(mynum);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (item) => item.genre === 'History')
+
+ 
+  userBooks=books.filter((item)=>{
+    return item.title==='Book Nine';
+  
+})
+  console.log(userBooks);
